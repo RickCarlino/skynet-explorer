faa9d5762e350b927b3bf1284c99f0cd0e720a54
rick carlino <rick.carlino@gmail.com>
2014-01-09 07:17:24 -0700

    Stubbing out models. Created status badge. Added device UUID list

--- static/script.js
+++ static/script.js
@@ -1,6 +1,31 @@
-// Add your javascript here
+var Device = function () {
+  self = this;
+  self.uuid = ko.observable('ad698900-2546-11e3-87fb-c560cb0ca47b');
+};
+
+var SkyNetStatus = function () {
+  self = this;
+  self.message = ko.observable('online');
+};
+
+var DeviceEventList = function () {
+  self = this;
+  self.attr = ko.observable('attr');
+};
+
+var DeviceList = function (ky, vlue) {
+  self = this;
+  self.key = ko.observable(ky);
+  self.val = ko.observable(vlue);
+};
+
+var App = function () {
+  this.networkStatus = new SkyNetStatus();
+  this.device = new Device();
+  this.deviceList = new DeviceList('type', 'drone');
+  this.deviceEvents = new DeviceEventList(self.uuid);
+};
+
 $(function(){
-  // $("h1").animate({
-  //   "margin-left": "100px"
-  // }, "slow");
+  ko.applyBindings(new App());
 });
